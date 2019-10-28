6be850f034bac505503c25deac7fa3c4afca5596
PuneetSivananda <psivananda@chatsoft.com>
2019-10-28 14:49:18 +0530

    add Boilderplate class component for exercise

---
+++ src/screens/ImageScreen.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import {Text, StyleSheet} from 'reac-native'
+
+export default class ImageScreen extends React.Component{
+    constructor(props){
+        super(props)
+    }
+
+    render(){
+
+    }
+}
+
+const style = StyleSheet.create({
+    
+})

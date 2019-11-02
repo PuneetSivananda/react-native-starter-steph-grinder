521d165b79b742e54611d471cc9f912caa53e771
PuneetSivananda <psivananda@chatsoft.com>
2019-11-02 13:38:10 +0530

    updated color changer with rules

--- src/components/ColorCounter.js
+++ src/components/ColorCounter.js
@@ -1,15 +1,14 @@
 import React, {useState} from 'react'
 import {View,Text, StyleSheet, Button, FlatList} from 'react-native'
 
-export default ColorCounter = () => {
+export default ColorCounter = (props) => {
     // const [colors, setColors] = useState([]);
 
     return <View>
-        <Text>Red</Text>
-        <Button title="Increase Red" />
-        <Button title="Decrease Red" />
-       
-    </View>
+        <Text>{props.color}</Text>
+        <Button onPress={()=>{props.onIncrease()}} title={`Increase ${props.color}`} />
+        <Button onPress={()=>{props.onDecrease()}} title={`Decrease ${props.color}`} />
+        </View>
 }
 
 const stle = StyleSheet.create({
--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -2,13 +2,57 @@ import React, {useState} from 'react'
 import {View,Text, StyleSheet, Button, FlatList} from 'react-native'
 import ColorCounter from "../components/ColorCounter"
 
-export default SquareScreen = () => {
-    const [colors, setColors] = useState([]);
+const COLOR_INCREMENT=25
 
+export default SquareScreen = () => {
+    const [red, setRed] = useState(0);
+    const [green, setGreen] = useState(0);
+    const [blue, setBlue] = useState(0);
+    console.log(red)
+    console.log(green)
+    console.log(blue)
+    const setColor = (color, change) =>{
+        if (color === "red"){
+            if (red + change > 255 || red + change < 0 ){
+                return
+            }
+            else {
+                setRed(red + change)
+            }
+        }
+        if (color === "blue"){
+            if (blue + change > 255 || blue + change < 0 ){
+                return
+            }
+            else {
+                setBlue(blue + change)
+            }
+        }
+        if (color === "green"){
+            if (green + change > 255 || green + change < 0 ){
+                return
+            }
+            else {
+                setGreen(green + change)
+            }
+        }
+    }
+    
     return <View>
-        <ColorCounter/>
-        <ColorCounter/>
-        <ColorCounter/>       
+        <ColorCounter color="Red" 
+            onIncrease={()=>{setColor("red",COLOR_INCREMENT)}}
+            onDecrease={()=>{setColor("red",-1 * COLOR_INCREMENT)}}
+            />
+        <ColorCounter color="Blue"
+            onIncrease={()=>{setColor("blue",COLOR_INCREMENT)}}
+            onDecrease={()=>{setColor("blue",-1 * COLOR_INCREMENT)}}
+            />
+        <ColorCounter color="Green"
+            onIncrease={()=>{setColor("green",COLOR_INCREMENT)}}
+            onDecrease={()=>{setColor("green",-1 * COLOR_INCREMENT)}}
+            />       
+        <View style={{height:150, width:150, backgroundColor:`rgb(${red}, ${green}, ${blue})`}}/>
+            
     </View>
 }
 

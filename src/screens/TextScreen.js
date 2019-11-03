0e4c9c69b9a6d3e42faf934d0a956d4b235c888c
PuneetSivananda <psivananda@chatsoft.com>
2019-11-03 16:57:02 +0530

    updates to text input , ui updates

--- src/screens/TextScreen.js
+++ src/screens/TextScreen.js
@@ -1,17 +1,25 @@
 import React, {useState, useReducer} from 'react'
-import {View, Text, StyleSheet, Image, Button} from 'react-native'
+import {View, Text, StyleSheet, TextInput, Image, Button} from 'react-native'
 
 export default TextScreen = () => {
     return <View>
         <Text>
             Text Screen
         </Text>
+        <TextInput 
+            style={textStyles.input} 
+            autoCorrect={false}
+            autoCapitalize="none"
+        />
     </View>
     
 }
 
-const counterStyles = StyleSheet.create({
-    style:{
+const textStyles = StyleSheet.create({
+    input:{
+        margin:15,
+        borderColor: "black",
+        borderWidth:1
     }   
 })
 

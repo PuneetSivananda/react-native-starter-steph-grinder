3744506fed83b053cc2a5248db76fc4cf18839ae
PuneetSivananda <psivananda@chatsoft.com>
2019-11-03 14:42:25 +0530

    Counter Screen with Redux implement Business rules

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,16 +1,34 @@
-import React, {useState} from 'react'
+import React, {useState, useReducer} from 'react'
 import {View, Text, StyleSheet, Image, Button} from 'react-native'
 // To do add reducer but before that refactor to a function component
-
+//Reducer function
+const INCREMENT = 1 
+const reducer = (state, action)=>{
+    switch (action.type){
+        case 'change_count_add':
+            return state.count >= 15
+            ? state 
+            : {...state, count:state.count + action.payload * 1}
+        case 'change_count_minus':
+            return state.count <= 0
+            ? state
+            : {...state, count:state.count + action.payload * -1}
+        default:
+            return state
+    }
+}
 export default CounterScreen = () => {
-    const [counter, setCounter] = useState(0);
+    // const [counter, setCounter] = useState(0);
+    const [state, dispatch] = useReducer(reducer, {count:0})
+    const {count} = state 
     
     return <View>
         <Button
             title="Increase"
             onPress={() => {
                 // this.state.counter++;
-                setCounter(counter+1)
+                // setCounter(counter+1)
+                dispatch({type:'change_count_add', payload:INCREMENT})
             }}
         />
 
@@ -18,13 +36,14 @@ export default CounterScreen = () => {
             title="Decrease"
             onPress={() => {
                 // this.state.counter--;
-                setCounter(counter-1)
+                // setCounter(counter-1)
                 // this.setState({ counter: this.state.counter - 1 })
+                dispatch({type:'change_count_minus', payload:INCREMENT})
             }}
         />
 
         <Text>
-            Current Count: {counter}
+            Current Count: {count}
         </Text>
     </View>
     

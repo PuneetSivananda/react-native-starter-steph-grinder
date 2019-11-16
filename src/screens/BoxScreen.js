74996c15737642c7787c549791d7c7aa53439e9b
PuneetSivananda <psivananda@chatsoft.com>
2019-11-16 21:56:00 +0530

    update align items

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -3,19 +3,23 @@ import {Text, View, StyleSheet} from 'react-native'
 
 const BoxScreen = () =>{
     return <View style={styles.viewStyle}>
-        <Text style={styles.textStyle}>Box Screen</Text>
+        <Text style={styles.textStyle}>Child 1</Text>
+        <Text style={styles.textStyle}>Child 2</Text>
+        <Text style={styles.textStyle}>Child 3</Text>
     </View>
 }
 const styles = StyleSheet.create(
     {
      viewStyle:{
          borderWidth:3,
-         borderColor:'black'
+         borderColor:'black',
+         alignItems:'flex-start'
      },
      textStyle:{
         borderColor:'red',
         margin:20,
-        borderWidth:10
+        borderWidth:2,
+        textAlign:"center"
      }
     }
 )

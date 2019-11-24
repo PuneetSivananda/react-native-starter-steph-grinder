11ea91b98630af9b7e2822507fffd9384c0b19c4
PuneetSivananda <psivananda@chatsoft.com>
2019-11-24 11:02:16 +0530

    flex direction row

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -3,9 +3,9 @@ import {Text, View, StyleSheet} from 'react-native'
 
 const BoxScreen = () =>{
     return <View style={styles.viewStyle}>
-        <Text style={styles.textStyle}>Child 1</Text>
-        <Text style={styles.textStyle}>Child 2</Text>
-        <Text style={styles.textStyle}>Child 3</Text>
+        <Text style={styles.textOneStyle}>Child 1</Text>
+        <Text style={styles.textTwoStyle}>Child 2</Text>
+        <Text style={styles.textThreeStyle}>Child 3</Text>
     </View>
 }
 const styles = StyleSheet.create(
@@ -14,15 +14,20 @@ const styles = StyleSheet.create(
          borderWidth:3,
          borderColor:'black',
          height:200,
-         flexDirection:'row',
-         justifyContent:'space-around'
+         flexDirection:"row"
      },
-     textStyle:{
+     textOneStyle:{
         borderColor:'red',
-        // margin:20,
-        borderWidth:2,
-        // textAlign:"center",
-        // flexDirection:'column'
+        borderWidth:3,
+     },
+     textTwoStyle:{
+        borderColor:'red',
+        borderWidth:3,
+        flex:1
+     },
+     textThreeStyle:{
+        borderColor:'red',
+        borderWidth:3,
      }
     }
 )

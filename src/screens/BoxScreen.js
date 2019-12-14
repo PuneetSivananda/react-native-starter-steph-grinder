7e634c6e327ddf93a69ce90d36aabd9811d95754
PuneetSivananda <psivananda@chatsoft.com>
2019-12-14 21:56:48 +0530

    Finish Box Screen Assignment

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -3,47 +3,40 @@ import {Text, View, StyleSheet} from 'react-native'
 
 const BoxScreen = () =>{
     return <>
-        <View style={styles.viewStyle1}>
-            <Text style={styles.textOneStyle}></Text>
-            <Text style={styles.textTwoStyle}></Text>
-        </View>
-        <View style={styles.viewStyle2}>
-            <Text style={styles.textThreeStyle}></Text>
+        <View style={styles.parentStyle}>
+            <Text style={styles.viewOneStyle}></Text>
+            <Text style={styles.viewTwoStyle}></Text>
+            <Text style={styles.viewThreeStyle}></Text>
         </View>
         </>
 }
 const styles = StyleSheet.create(
     {
-     viewStyle1:{
+     parentStyle:{
          borderWidth:3,
          borderColor:'black',
-         height:200,
-         flexDirection:'row'
+         height:100,
+         flexDirection:'row',
+         justifyContent:"space-between"
      },
-     viewStyle2:{
-        borderWidth:1,
-        borderColor:'black',
-        height:200,
-        flexDirection:'column'
-    },
-     textOneStyle:{
-        borderColor:'transparent',
-        borderWidth:0,
-        backgroundColor:'red',
-        flex:3
+     viewOneStyle:{
+        height:50,
+        width:50,
+        backgroundColor:'red'
+        
      },
-     textTwoStyle:{
-        borderColor:'transparent',
-        borderWidth:0,
+     viewTwoStyle:{
+        height:50,
+        width:50,
         backgroundColor:'green',
-        flex:3,
-        ...StyleSheet.absoluteFillObject
-     },
-     textThreeStyle:{
-        borderColor:'transparent',
-        borderWidth:0,
-        backgroundColor:'blue',
-        flex:3
+        alignSelf:'flex-end'
+        
+    },
+     viewThreeStyle:{
+        height:50,
+        width:50,
+        backgroundColor:'purple'
+        
     }
 }
 )
